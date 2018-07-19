c603ab36a2e051af38eca9837d14c53e91b5cc3b
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-07-19 14:31:18 +0200

    add jest

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,64 +1,21 @@
-/* global describe, it, before */
+import { Client } from '../lib/byteball';
 
-import chai from 'chai';
-import { Client, utils } from '../lib/byteball.js';
+describe('Client', () => {
+  let client = null;
 
-chai.expect();
-const expect = chai.expect;
-let lib;
-
-describe('Given an instance of my Dog library', () => {
-  before(() => {
-    lib = utils;
+  beforeAll(() => {
+    client = new Client('wss://byteball.fr/bb');
   });
-  describe('when I need the name', () => {
-    it('should return the name', () => {
-      expect('Dog').to.be.equal('Dog');
-    });
+
+  it('should create client', () => {
+    expect(typeof client).toBe('object');
+  });
+
+  it('should support callbacks', (done) => {
+    client.getJoint('12345', () => done());
+  });
+
+  it('should support promises', (done) => {
+    client.getJoint('12345').then(() => done());
   });
 });
-
-const client = new Client('wss://byteball.org/bb');
-
-client.getJoint('QUsS+EhZILpetbOPrkILsR4UTEd4IP9vxoTOY4vYUBg=').then((result) => {
-  console.log(2, JSON.stringify(result));
-}).catch((err) => {
-  console.log(2, err);
-});
-
-client.getJoint('123465', (err, result) => {
-  console.log(1, err, JSON.stringify(result));
-});
-
-client.hubGetBots((err, result) => {
-  console.log(3, err, JSON.stringify(result));
-});
-
-client.hubGetBots().then((result) => {
-  console.log(4, JSON.stringify(result));
-}).catch((err) => {
-  console.log(4, err);
-});
-
-client.hubGetBots(null, (err, result) => {
-  console.log(5, err, JSON.stringify(result));
-});
-
-client.hubGetBots(null).then((result) => {
-  console.log(6, JSON.stringify(result));
-}).catch((err) => {
-  console.log(6, err);
-});
-
-client.lightGetAttestations({ address: 'NX2BTV43XN6BOTCYZUUFU6TK7DVOC4LU' }, (err, result) => {
-  console.log(7, err, JSON.stringify(result));
-});
-
-client.lightGetAttestation({
-  attestor_address: 'JEDZYC2HMGDBIDQKG3XSTXUSHMCBK725',
-  field: 'steem_username',
-  value: 'sekhmet',
-}, (err, result) => {
-  console.log(8, err, JSON.stringify(result));
-});
-
