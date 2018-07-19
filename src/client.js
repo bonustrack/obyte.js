89ffa8b22dd528e0030053a3f52a2cbcf10fd07c
adcpm <fabien@bonustrack.co>
2018-07-20 00:30:24 +0200

    Fix lint error

--- src/client.js
+++ src/client.js
@@ -5,9 +5,13 @@ export default class Client {
   constructor(address) {
     this.client = new KbyteClient(address);
     Object.keys(api).forEach(name => {
-      const nameWithCamelCase = name.split('/').pop().split('_')
+      const nameWithCamelCase = name
+        .split('/')
+        .pop()
+        .split('_')
         .map(p => p.charAt(0).toUpperCase() + p.slice(1))
-        .join('').replace(/^\w/, c => c.toLowerCase());
+        .join('')
+        .replace(/^\w/, c => c.toLowerCase());
       this[nameWithCamelCase] = (params, cb) => {
         if (!api[name].params && typeof params === 'function') {
           cb = params; // eslint-disable-line no-param-reassign
