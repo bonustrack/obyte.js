c1b9b05272c2b31ebf51df4be09cf1ac1e01bd66
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-07-19 14:59:36 +0200

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
-  extends: 'airbnb-base',
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   env: {
     node: true,
     browser: true,
     jest: true,
   },
-}
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
