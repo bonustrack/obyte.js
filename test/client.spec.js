00692ce494360de6819e54e4a8c0071a3b467b93
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-07-27 16:16:30 +0200

    cache witnesses (#18)

--- test/client.spec.js
+++ test/client.spec.js
@@ -30,4 +30,33 @@ describe('Client', () => {
   it('should catch error with promises', done => {
     client.getParentsAndLastBallAndWitnessListUnit().catch(() => done());
   });
+
+  it('should cache witnesses', async () => {
+    const witnesses = [
+      'BVVJ2K7ENPZZ3VYZFWQWK7ISPCATFIW3',
+      'DJMMI5JYA5BWQYSXDPRZJVLW3UGL3GJS',
+      'FOPUBEUPBC6YLIQDLKL6EW775BMV7YOH',
+      'GFK3RDAPQLLNCMQEVGGD2KCPZTLSG3HN',
+      'H5EZTQE7ABFH27AUDTQFMZIALANK6RBG',
+      'I2ADHGP4HL6J37NQAD73J7E5SKFIXJOT',
+      'JEDZYC2HMGDBIDQKG3XSTXUSHMCBK725',
+      'JPQKPRI5FMTQRJF4ZZMYZYDQVRD55OTC',
+      'OYW2XTDKSNKGSEZ27LMGNOPJSYIXHBHC',
+      'S7N5FE42F6ONPNDQLCF64E2MGFYKQR2I',
+      'TKT4UESIKTTRALRRLWS4SENSTJX6ODCW',
+      'UENJPVZ7HVHM6QGVGT6MWOJGGRTUTJXQ',
+    ];
+
+    client.getWitnesses = jest.fn().mockReturnValue(witnesses);
+
+    let receivedWitnesses = await client.getCachedWitnesses();
+    expect(receivedWitnesses).toEqual(witnesses);
+    expect(client.getWitnesses).toHaveBeenCalled();
+
+    client.getWitnesses.mockClear();
+
+    receivedWitnesses = await client.getCachedWitnesses();
+    expect(receivedWitnesses).toEqual(witnesses);
+    expect(client.getWitnesses).not.toHaveBeenCalled();
+  });
 });
