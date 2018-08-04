2791b5552f9eaf38a672c0816f1d2855568d9996
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-08-04 09:55:33 +0200

    update getHistory types

--- index.d.ts
+++ index.d.ts
@@ -120,7 +120,7 @@ declare namespace Byteball {
     ): Promise<string>;
 
     getHistory(
-      params: { witnesses: string[]; addresses: string[] },
+      params: { witnesses: string[]; addresses?: string[], requested_joints?: string[] },
       callback?: (err: null | string, result: string | null) => void,
     ): Promise<HistoryResponse>;
 
