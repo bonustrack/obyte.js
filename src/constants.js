60546baf16c089eeb60ca334238390b0bf5d3f2e
papabyte <bcreze@protonmail.com>
2020-06-28 14:42:02 +0200

    remove unused constant

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,6 @@
 export const DEFAULT_NODE = 'wss://obyte.org/bb';
 export const VERSION = '3.0';
 export const VERSION_TESTNET = '3.0t';
-export const VERSION_WITHOUT_TIMESTAMP = '1.0';
 export const VERSION_WITHOUT_KEY_SIZES = '2.0';
 export const ALT = '1';
 export const ALT_TESTNET = '2';
